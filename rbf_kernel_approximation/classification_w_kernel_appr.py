c57cc066b711cdff467e1a444da13d45e534489a
queqichao <colin.que@gmail.com>
2014-09-25 14:03:16 -0400

    + Add a config file for kernel approximation experiments. And rename the patches_clustering.py to a more self explanatory name.

--- rbf_kernel_approximation/classification_w_kernel_appr.py
+++ rbf_kernel_approximation/classification_w_kernel_appr.py
@@ -26,7 +26,7 @@ elif data_config["name"] == "cifar10":
 image_data = dataset.ImageDataSet(images.astype(np.uint8))
 
 classifiers = config["classifiers"]
-results = [0,0]
+results = [0, 0, 0, 0]
 data = image_data.to_array().astype(np.float)
 data = dataset.SupervisedDataSet(data, labels, config["num_training"])
 
@@ -34,7 +34,13 @@ classifiers[0]["n_jobs"] = 1
 results[0] = classifier_help.evaluation_classifier(data, classifiers[0], True, 5, fit_params={"kernel_training_data": image_data.to_array().astype(np.float)})
 
 classifiers[1]["n_jobs"] = 1
-results[1] = classifier_help.evaluation_classifier(data, classifiers[1], True, 5)
+results[1] = classifier_help.evaluation_classifier(data, classifiers[0], True, 5, fit_params={"kernel_training_data": image_data.to_array().astype(np.float)})
+
+classifiers[2]["n_jobs"] = 1
+results[2] = classifier_help.evaluation_classifier(data, classifiers[0], True, 5, fit_params={"kernel_training_data": image_data.to_array().astype(np.float)})
+
+classifiers[3]["n_jobs"] = 1
+results[3] = classifier_help.evaluation_classifier(data, classifiers[1], True, 5)
 
 result_str = ''
 for j, result in enumerate(results):
