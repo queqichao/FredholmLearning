+ Fix Bug in classification_w_kernel_appr.py: wrong index for classifiers. + Add option of doing zero centering for 20 news group after SVD. + Fix Bug in laprlsc.py: not adding identity regularization term and not computing labeled correctly.

fredholm_kernel_learning/laprlsc.py
@@ -25,7 +25,8 @@ class LapRLSC(BaseL2KernelClassifier):
     num_data = X.shape[0] + unlabeled_data.shape[0]
     num_labeled = X.shape[0]
     num_unlabeled = unlabeled_data.shape[0]
-    labeled = range(num_labeled)
+    labeled = np.zeros((num_data,), dtype=np.float32)
+    labeled[0:num_labeled] = 1.0;
     if issparse(X):
       self.X_ = vstack((util.cast_to_float32(X),
                         util.cast_to_float32(unlabeled_data)), format='csr')
@@ -36,7 +37,7 @@ class LapRLSC(BaseL2KernelClassifier):
         self.rbf_gamma if self.rbf_gamma is not None else 1.0 / X.shape[1])
     kernel_matrix_ = rbf_kernel(self.X_, gamma=self.rbf_gamma_)
     I = np.identity(num_data, dtype=np.float32)
-    A = np.dot(np.diag(labeled.astype(np.float32)), kernel_matrix_)
+    A = np.dot(np.diag(labeled), kernel_matrix_) + self.nu * I
     if self.nu2 != 0:
       laplacian_x_kernel = np.dot(graph_laplacian(
           kernel_matrix_, normed=self.normalize_laplacian), kernel_matrix_)

rbf_kernel_approximation/classification_w_kernel_appr.py
@@ -9,6 +9,8 @@ from sklearn.linear_model import RidgeClassifier
 import os
 from fredholm_kernel_learning import L2KernelClassifier
 from fredholm_kernel_learning import classifier_help
+import getpass
+import socket
 
 parser = argparse.ArgumentParser()
 parser.add_argument('config_file', help='Config file.', type=str)
@@ -34,13 +36,13 @@ classifiers[0]["n_jobs"] = 1
 results[0] = classifier_help.evaluation_classifier(data, classifiers[0], True, 5, fit_params={"kernel_training_data": image_data.to_array().astype(np.float)})
 
 classifiers[1]["n_jobs"] = 1
-results[1] = classifier_help.evaluation_classifier(data, classifiers[0], True, 5, fit_params={"kernel_training_data": image_data.to_array().astype(np.float)})
+results[1] = classifier_help.evaluation_classifier(data, classifiers[1], True, 5, fit_params={"kernel_training_data": image_data.to_array().astype(np.float)})
 
 classifiers[2]["n_jobs"] = 1
-results[2] = classifier_help.evaluation_classifier(data, classifiers[0], True, 5, fit_params={"kernel_training_data": image_data.to_array().astype(np.float)})
+results[2] = classifier_help.evaluation_classifier(data, classifiers[2], True, 5, fit_params={"kernel_training_data": image_data.to_array().astype(np.float)})
 
 classifiers[3]["n_jobs"] = 1
-results[3] = classifier_help.evaluation_classifier(data, classifiers[1], True, 5)
+results[3] = classifier_help.evaluation_classifier(data, classifiers[3], True, 5)
 
 result_str = ''
 for j, result in enumerate(results):