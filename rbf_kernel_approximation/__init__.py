1f5f9633a8855814d3db94acf1e697f7bf81eb0e
queqichao <colin.que@gmail.com>
2014-10-02 13:30:01 -0400

    + Adapt from the original repo.

--- rbf_kernel_approximation/__init__.py
+++
@@ -1,9 +0,0 @@
-from .rbf_kernel_regression import KMeansRegression
-from .rbf_kernel_regression import RBFKernelApprClassifier
-from .rbf_kernel_regression import _loss_fun as loss_fun
-from .rbf_kernel_regression import _loss_fun_prime as loss_fun_prime
-
-__all__ = ['KMeansRegression',
-           'RBFKernelApprClassifier',
-           'kernel_approximation_help',
-           'loss_fun', 'loss_fun_prime']
